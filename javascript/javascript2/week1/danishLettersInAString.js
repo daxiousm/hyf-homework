4b9b50d4541c5679f0e5046e13930095aad3c192
Daxious <daxiousm@gmail.com>
2020-06-22 13:15:12 +0200

    feedback implemented

--- javascript/javascript2/week1/danishLettersInAString.js
+++ javascript/javascript2/week1/danishLettersInAString.js
@@ -1,15 +1,22 @@
-const danishString = ["Jeg har en blå bil"];
+const danishString = ["Jeg har en blÅ bil"];
 const danshString1 = danishString.toString();
-const danishString2 = ["Blå grød med rød bær"];
+const danishString2 = ["Blå grØd med rød bær"];
 const danshString3 = danishString2.toString();
 function char_count(str) {
   letter = ["æ", "ø", "å"];
   let letter_Count = 0;
   for (let position = 0; position < str.length; position++) {
     for (let j = 0; j < letter.length; j++)
-      if (str.charAt(position) === letter[j]) {
+      if (
+        str.toLocaleLowerCase().charAt(position) ===
+        letter[j].toLocaleLowerCase()
+      ) {
         letter_Count += 1;
-        console.log(`${letter[j]}:${str.split(letter[j]).length - 1}`);
+        console.log(
+          `${letter[j].toLocaleLowerCase()}:${
+            str.split(letter[j].toLocaleLowerCase()).length - 1
+          }`
+        );
       }
   }
   console.log(`total:${letter_Count}`);
