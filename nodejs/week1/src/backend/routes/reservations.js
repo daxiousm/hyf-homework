84bb9a42ab2161ed92028930a660feacf997beb8
Daxious <daxiousm@gmail.com>
2020-09-15 11:11:51 +0200

    m

--- nodejs/week1/src/backend/routes/reservations.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reservations = require("../data/reservations.json");
-
-router.get("/reservations", (req, res) => {
-  res(reservations);
-});
-
-module.exports = router;
--- nodejs/week1/src/backend/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app.js");
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-);
-module.exports = server;
