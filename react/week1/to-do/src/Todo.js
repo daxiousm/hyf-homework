22492bc24be45dd2147b931c8d31c3c503a57962
daxiousm <62550927+daxiousm@users.noreply.github.com>
2020-10-19 13:00:25 +0200

    Update react/week1/to-do/src/Todo.js
    Co-authored-by: Hugo Leote <hleote@gmail.com>

--- react/week1/to-do/src/Todo.js
+++ react/week1/to-do/src/Todo.js
@@ -1,42 +1,24 @@
 import React from "react";
 import "./Todo.css";
 
-const ToDos = (props) => {
-  console.log(props);
-  const { descriptions, deadlines } = props;
-
-  const MyDescriptions = (props) => {
-    return (
-      <div className="description">
-        <p>{props.description}</p>
-      </div>
-    );
-  };
-
-  const MyDeadLines = (props) => {
-    return (
-      <div>
-        <p>{props.deadline}</p>
-      </div>
-    );
-  };
+const Todo = ({ description, deadline }) => {
 
   return (
     <div className="tasks">
       <div className="dos">
-        <strong className="desc">Descriptions:</strong>
-        {descriptions.map((description) => {
-          return <MyDescriptions key={description} description={description} />;
-        })}
+        <strong className="desc">Description:</strong>
+          <div className="description">
+            <p>{props.description}</p>
+          </div>
       </div>
       <div className="dos">
-        <strong className="deadline">Deadlines:</strong>
-        {deadlines.map((deadline) => {
-          return <MyDeadLines key={deadline} deadline={deadline} />;
-        })}
+        <strong className="deadline">Deadline:</strong>
+          <div className="deadline">
+            <p>{props.deadline}</p>
+          </div>
       </div>
     </div>
   );
 };
 
-export default ToDos;
+export default Todo;
