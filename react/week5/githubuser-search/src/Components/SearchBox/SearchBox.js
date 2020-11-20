a7117ad5d973d1fcd2af5607bd45e08b98ecf070
daxiousm <62550927+daxiousm@users.noreply.github.com>
2020-11-20 15:04:34 +0100

    Update react/week5/githubuser-search/src/Components/SearchBox/SearchBox.js
    Co-authored-by: benna100 <benna100@users.noreply.github.com>

--- react/week5/githubuser-search/src/Components/SearchBox/SearchBox.js
+++ react/week5/githubuser-search/src/Components/SearchBox/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBox = ( {searchChange} ) => {
     return (
         <div className='pa1 tc'>
-            <form onSubmit={searchChange}>
+            <form onSubmit={searchSubmitted}>
                 <div className="pa2">
                     <input
                         className="pa3 ba b--green bg-lightest-blue"
